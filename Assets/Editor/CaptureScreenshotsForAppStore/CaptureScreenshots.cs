76881f7aba0ddaf63a3af960fc3c4f0f05405a41
takanakahiko <takanakahiko@gmail.com>
2020-05-14 07:18:17 +0900

    first commit

---
+++ Assets/Editor/CaptureScreenshotsForAppStore/CaptureScreenshots.cs
@@ -0,0 +1,80 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Reflection;
+
+using Kyusyukeigo.Helper;
+using UnityExtensions;
+using System.IO;
+
+namespace takanakahiko.Utility {
+
+    public class CaptureScreenshotsForAppStore : Editor {
+
+        class GameViewSize : GameViewSizeHelper.GameViewSize {
+            public GameViewSize(int width, int height, string baseText) {
+                type = GameViewSizeHelper.GameViewSizeType.FixedResolution;
+                this.width = width;
+                this.height = height;
+                this.baseText = baseText;
+            }
+        }
+
+        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot1")]
+        private static void CaptureScreenshot1() {
+            EditorCoroutine.Start(CaptureScreenshot(1));
+        }
+
+        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot2")]
+        private static void CaptureScreenshot2() {
+            EditorCoroutine.Start(CaptureScreenshot(2));
+        }
+
+        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot3")]
+        private static void CaptureScreenshot3() {
+            EditorCoroutine.Start(CaptureScreenshot(3));
+        }
+
+        static IEnumerator CaptureScreenshot(int number) {
+
+            string directoryName = "screenshots";
+            if (!Directory.Exists(directoryName))
+                Directory.CreateDirectory(directoryName);
+
+            var assembly = typeof(EditorWindow).Assembly;
+            var groupType = currentSizeGroupType(assembly);
+            var type = assembly.GetType("UnityEditor.GameView");
+            var gameview = EditorWindow.GetWindow(type);
+
+            GameViewSize[] customSizes =  {
+                new GameViewSize(1242, 2688, "6.5"),
+                new GameViewSize(1242, 2208, "5.5"),
+                new GameViewSize(2048, 2732, "12.9"),
+            };
+
+            for (int i = 0; i < customSizes.Length; i++) {
+                var customSize = customSizes[i];
+                if (!GameViewSizeHelper.Contains(groupType, customSize))
+                    GameViewSizeHelper.AddCustomSize(groupType, customSize);
+                GameViewSizeHelper.ChangeGameViewSize(groupType, customSize);
+                var filename = $"{directoryName}/{customSize.baseText}_{number}.png";
+                EditorApplication.Step();
+                EditorApplication.Step();
+                ScreenCapture.CaptureScreenshot(filename);
+                gameview.Repaint();
+                Debug.Log(">> CaptureScreenshotsForAppStore : save to " + filename);
+                //GameViewSizeHelper.RemoveCustomSize(groupType, customSize);
+                yield return null;
+            }
+        }
+
+        static GameViewSizeGroupType currentSizeGroupType(Assembly assembly) {
+            Type gameView = assembly.GetType("UnityEditor.GameView");
+            PropertyInfo currentSizeGroupType = gameView.GetProperty("currentSizeGroupType", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+            return (GameViewSizeGroupType)currentSizeGroupType.GetValue(EditorWindow.GetWindow(gameView), null);
+        }
+
+    }
+}
