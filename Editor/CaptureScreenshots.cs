4a6204f9c3ff47c9fd81989f1431153440c9e5b0
cllightz <cllightz@users.noreply.github.com>
2020-06-17 12:15:28 +0900

    リファクタリングしました (#10)
    * usingディレクティブの並べ替え
    
    * 名前空間の変更
    
    * XMLドキュメントコメントの追加
    
    * 開き波括弧の改行位置をVS標準に合わせた & GameViewSizeのコンストラクタをinternal化
    
    * 解像度定義をフィールド化 & foreach化
    
    * MenuItem用のメソッドを末尾に移動
    
    * 開き波括弧の改行位置をVS標準に合わせた
    
    * 文字列補間式に変更 & 古いコメントアウト部分を削除
    
    * ifブロックをきちんと書いた
    
    * Path.Combineを活用
    
    * メソッドとローカル変数の識別子を一般的な感じに変更

--- Editor/CaptureScreenshots.cs
+++ Editor/CaptureScreenshots.cs
@@ -1,20 +1,22 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-using System;
-using System.Collections;
-using System.Reflection;
-
-using Kyusyukeigo.Helper;
-using UnityExtensions;
+﻿using System.Collections;
 using System.IO;
+using System.Reflection;
+using Kyusyukeigo.Helper;
+using UnityEditor;
+using UnityEngine;
+using UnityExtensions;
 
-namespace takanakahiko.Utility {
-
-    public class CaptureScreenshotsForAppStore : Editor {
-
-        class GameViewSize : GameViewSizeHelper.GameViewSize {
-            public GameViewSize(int width, int height, string baseText) {
+namespace CaptureScreenshotsForAppStore
+{
+    /// <summary>
+    /// The Unity editor extension to capture screenshots for App Store 
+    /// </summary>
+    public class CaptureScreenshotsForAppStore : Editor
+    {
+        class GameViewSize : GameViewSizeHelper.GameViewSize
+        {
+            internal GameViewSize(int width, int height, string baseText)
+            {
                 type = GameViewSizeHelper.GameViewSizeType.FixedResolution;
                 this.width = width;
                 this.height = height;
@@ -22,59 +24,63 @@ namespace takanakahiko.Utility {
             }
         }
 
-        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot1")]
-        private static void CaptureScreenshot1() {
-            EditorCoroutine.Start(CaptureScreenshot(1));
-        }
+        static readonly GameViewSize[] _customSizes = {
+            new GameViewSize(1242, 2688, "6.5"),
+            new GameViewSize(1242, 2208, "5.5"),
+            new GameViewSize(2048, 2732, "12.9"),
+        };
 
-        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot2")]
-        private static void CaptureScreenshot2() {
-            EditorCoroutine.Start(CaptureScreenshot(2));
-        }
-
-        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot3")]
-        private static void CaptureScreenshot3() {
-            EditorCoroutine.Start(CaptureScreenshot(3));
-        }
-
-        static IEnumerator CaptureScreenshot(int number) {
-
+        static IEnumerator CaptureScreenshot(int number)
+        {
             string directoryName = "screenshots";
+
             if (!Directory.Exists(directoryName))
+            {
                 Directory.CreateDirectory(directoryName);
+            }
 
-            var assembly = typeof(EditorWindow).Assembly;
-            var groupType = currentSizeGroupType(assembly);
-            var type = assembly.GetType("UnityEditor.GameView");
-            var gameview = EditorWindow.GetWindow(type);
+            var editorWindowAssembly = typeof(EditorWindow).Assembly;
+            var currentSizeGroupType = GetCurrentSizeGroupType(editorWindowAssembly);
+            var gameViewType = editorWindowAssembly.GetType("UnityEditor.GameView");
+            var gameViewWindow = EditorWindow.GetWindow(gameViewType);
 
-            GameViewSize[] customSizes =  {
-                new GameViewSize(1242, 2688, "6.5"),
-                new GameViewSize(1242, 2208, "5.5"),
-                new GameViewSize(2048, 2732, "12.9"),
-            };
+            foreach (var customSize in _customSizes)
+            {
+                if (!GameViewSizeHelper.Contains(currentSizeGroupType, customSize))
+                {
+                    GameViewSizeHelper.AddCustomSize(currentSizeGroupType, customSize);
+                }
 
-            for (int i = 0; i < customSizes.Length; i++) {
-                var customSize = customSizes[i];
-                if (!GameViewSizeHelper.Contains(groupType, customSize))
-                    GameViewSizeHelper.AddCustomSize(groupType, customSize);
-                GameViewSizeHelper.ChangeGameViewSize(groupType, customSize);
-                var filename = $"{directoryName}/{customSize.baseText}_{number}.png";
+                GameViewSizeHelper.ChangeGameViewSize(currentSizeGroupType, customSize);
+                var filename = Path.Combine(directoryName, $"{customSize.baseText}_{number}.png");
                 EditorApplication.Step();
                 EditorApplication.Step();
                 ScreenCapture.CaptureScreenshot(filename);
-                gameview.Repaint();
-                Debug.Log(">> CaptureScreenshotsForAppStore : save to " + filename);
-                //GameViewSizeHelper.RemoveCustomSize(groupType, customSize);
+                gameViewWindow.Repaint();
+                Debug.Log($">> CaptureScreenshotsForAppStore : save to {filename}");
                 yield return null;
             }
         }
 
-        static GameViewSizeGroupType currentSizeGroupType(Assembly assembly) {
-            Type gameView = assembly.GetType("UnityEditor.GameView");
-            PropertyInfo currentSizeGroupType = gameView.GetProperty("currentSizeGroupType", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
-            return (GameViewSizeGroupType)currentSizeGroupType.GetValue(EditorWindow.GetWindow(gameView), null);
+        static GameViewSizeGroupType GetCurrentSizeGroupType(Assembly assembly)
+        {
+            var gameViewType = assembly.GetType("UnityEditor.GameView");
+            var currentSizeGroupType = gameViewType.GetProperty("currentSizeGroupType", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+            return (GameViewSizeGroupType)currentSizeGroupType.GetValue(EditorWindow.GetWindow(gameViewType), null);
         }
 
+        #region MenuItem methods
+        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot1")]
+        static void CaptureScreenshot1()
+            => EditorCoroutine.Start(CaptureScreenshot(1));
+
+        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot2")]
+        static void CaptureScreenshot2()
+            => EditorCoroutine.Start(CaptureScreenshot(2));
+
+        [MenuItem("CaptureScreenshotsForAppStore/CaptureScreenshot3")]
+        static void CaptureScreenshot3()
+            => EditorCoroutine.Start(CaptureScreenshot(3));
+        #endregion
     }
 }
